8a0918623213320b28cdb4659bb6a9f41897e0b9
olakunle.olatunji <tjomas20@gmail.com>
2024-12-23 18:32:46 +0100

    chore: add essential folders and module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,17 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    rules: {
+      'vue/max-attributes-per-line': 'off',
+      'vue/html-self-closing': ['warn', {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+      }],
+    },
+    ignores: ['*.css', '*.png', '*.ico', '*.txt', 'src/public/'],
+  },
 )
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode'
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui',
+  },
 })
