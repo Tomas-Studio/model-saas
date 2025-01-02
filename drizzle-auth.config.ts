d17dbc0f8d26f15d351edb58b38cf99e107c4619
olakunle.olatunji <tjomas20@gmail.com>
2025-01-02 02:07:02 +0100

    chore: configure both drizzle config files

--- drizzle-auth.config.ts
+++ drizzle-auth.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'drizzle-kit'
-import { consola } from 'consola'
+
+if (!process.env.NUXT_TURSO_AUTH_DATABASE_NAME) {
+  throw new Error("NUXT_TURSO_AUTH_DATABASE_NAME is missing")
+}
+
+if (!process.env.NUXT_TURSO_ORG) {
+  throw new Error("NUXT_TURSO_ORG is missing")
+}
+
+if (!process.env.NUXT_TURSO_GROUP_AUTH_TOKEN) {
+  throw new Error("NUXT_TURSO_GROUP_AUTH_TOKEN is missing")
+}
+
+const url = `libsql://${process.env.NUXT_TURSO_AUTH_DATABASE_NAME}-${process.env.NUXT_TURSO_ORG}.turso.io`
 
 export default defineConfig({
   dialect: 'turso',
   schema: './server/database/auth/schema',
   out: './server/database/auth/migrations',
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.NUXT_TURSO_GROUP_AUTH_TOKEN!,
+    url,
+    authToken: process.env.NUXT_TURSO_GROUP_AUTH_TOKEN,
   },
 }) 
--- drizzle-tenant.config.ts
+++ drizzle-tenant.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'drizzle-kit'
 
+if (!process.env.NUXT_TURSO_TENANT_DATABASE_NAME) {
+  throw new Error("NUXT_TURSO_TENANT_DATABASE_NAME is missing")
+}
+
+if (!process.env.NUXT_TURSO_ORG) {
+  throw new Error("NUXT_TURSO_ORG is missing")
+}
+
+if (!process.env.NUXT_TURSO_GROUP_AUTH_TOKEN) {
+  throw new Error("NUXT_TURSO_GROUP_AUTH_TOKEN is missing")
+}
+
+const url = `libsql://${process.env.NUXT_TURSO_TENANT_DATABASE_NAME}-${process.env.NUXT_TURSO_ORG}.turso.io`
+
 export default defineConfig({
   dialect: 'turso',
   schema: './server/database/tenant/schema',
   out: './server/database/tenant/migrations',
+  dbCredentials: {
+    url,
+    authToken: process.env.NUXT_TURSO_GROUP_AUTH_TOKEN,
+  },
 })
